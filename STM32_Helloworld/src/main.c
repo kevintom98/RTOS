3bef291e94b6e896979b9dd5711cc60ea975eec4
Kevin Tom <kevintom98@gmail.com>
2021-02-25 17:22:23 +0530

    Update main.c

--- STM32_Helloworld/src/main.c
+++ STM32_Helloworld/src/main.c
@@ -1,12 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
 /*
  * Author: Kevin Tom (https://sites.google.com/view/kevintom)
  *
